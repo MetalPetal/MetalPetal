2bc07d3f61e3264f9069afc0036d0c63e8aa7bf5
YuAo <me@imyuao.com>
2017-08-07 23:59:11 +0800

    add render to cg stubs

--- Frameworks/MetalPetal/MTIContext+Rendering.h
+++ Frameworks/MetalPetal/MTIContext+Rendering.h
@@ -15,11 +15,13 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface MTIContext (Rendering)
 
-- (BOOL)renderImage:(MTIImage *)image toPixelBuffer:(CVPixelBufferRef)pixelBuffer error:(NSError **)error;
+- (BOOL)renderImage:(MTIImage *)image toCVPixelBuffer:(CVPixelBufferRef)pixelBuffer error:(NSError **)error NS_SWIFT_NAME(render(_:to:));
 
-- (BOOL)renderImage:(MTIImage *)image toDrawableWithRequest:(MTIDrawableRenderingRequest *)request error:(NSError **)error;
+- (BOOL)renderImage:(MTIImage *)image toDrawableWithRequest:(MTIDrawableRenderingRequest *)request error:(NSError **)error NS_SWIFT_NAME(render(_:toDrawableWithRequest:));
 
-- (nullable CIImage *)createCIImage:(MTIImage *)image error:(NSError **)error;
+- (nullable CIImage *)createCIImageFromImage:(MTIImage *)image error:(NSError **)error NS_SWIFT_NAME(makeCIImage(from:));
+
+- (nullable CGImageRef)createCGImageFromImage:(MTIImage *)image error:(NSError **)error NS_SWIFT_NAME(makeCGImage(from:));
 
 @end
 
