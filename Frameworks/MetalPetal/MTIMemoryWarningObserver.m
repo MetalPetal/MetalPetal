c9bd4abe7873e933b711dce0b1ca2e7d656aa289
YuAo <me@imyuao.com>
2018-12-13 14:43:48 +0800

    MTIMemoryWarningObserver tread-safety

--- Frameworks/MetalPetal/MTIMemoryWarningObserver.m
+++ Frameworks/MetalPetal/MTIMemoryWarningObserver.m
@@ -6,6 +6,7 @@
 //
 
 #import "MTIMemoryWarningObserver.h"
+#import "MTILock.h"
 
 #if __has_include(<UIKit/UIKit.h>)
 #import <UIKit/UIKit.h>
@@ -14,6 +15,7 @@
 @interface MTIMemoryWarningObserver ()
 
 @property (nonatomic, strong, readonly) NSHashTable *handlers;
+@property (nonatomic, strong, readonly) id<MTILocking> lock;
 
 @end
 
@@ -31,6 +33,7 @@
 - (instancetype)initForSharedObserver {
     if (self = [super init]) {
         _handlers = [NSHashTable weakObjectsHashTable];
+        _lock = MTILockCreate();
 #if __has_include(<UIKit/UIKit.h>)
         [NSNotificationCenter.defaultCenter addObserverForName:UIApplicationDidReceiveMemoryWarningNotification
                                                         object:nil
@@ -44,17 +47,23 @@
 }
 
 - (void)handleMemoryWarning {
+    [_lock lock];
     for (id<MTIMemoryWarningHandling> handler in _handlers) {
         [handler handleMemoryWarning];
     }
+    [_lock unlock];
 }
 
 - (void)addMemoryWarningHandler:(id<MTIMemoryWarningHandling>)memoryWarningHandler {
+    [_lock lock];
     [_handlers addObject:memoryWarningHandler];
+    [_lock unlock];
 }
 
 - (void)removeMemoryWarningHandler:(id<MTIMemoryWarningHandling>)memoryWarningHandler {
+    [_lock lock];
     [_handlers removeObject:memoryWarningHandler];
+    [_lock unlock];
 }
 
 + (void)addMemoryWarningHandler:(id<MTIMemoryWarningHandling>)memoryWarningHandler {
