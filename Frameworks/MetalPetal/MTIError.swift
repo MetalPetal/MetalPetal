204a49320bf76957945c633846910005a458f0c1
YuAo <me@imyuao.com>
2020-03-25 09:09:47 +0800

    workaround __derived_struct_equals in swift 5.2

---
+++ Frameworks/MetalPetal/MTIError.swift
@@ -0,0 +1,16 @@
+//
+//  MTIError.swift
+//  MetalPetal
+//
+//  Created by YuAo on 2020/3/25.
+//
+
+import Foundation
+
+#if SWIFT_PACKAGE
+import MetalPetalObjectiveC.Core
+#endif
+
+extension MTIError: Equatable {
+    
+}
