f03cf4683a9d1e4ca8e87b03bd612a7f748dece9
YuAo <me@imyuao.com>
2020-03-27 17:09:33 +0800

    update ci & test script to include tests for macCatalyst

--- test.sh
+++ test.sh
@@ -18,3 +18,15 @@ else
     xcodebuild build -scheme MetalPetal -destination 'platform=iOS Simulator,name=iPhone 11' -workspace .
     xcodebuild test -scheme MetalPetal -destination 'platform=iOS Simulator,name=iPhone 11' -workspace .
 fi
+
+echo "------------------"
+echo "Somke Test (macCatalyst)"
+echo "------------------"
+
+if hash xcpretty 2>/dev/null; then
+    xcodebuild build -scheme MetalPetal -destination 'platform=macOS,variant=Mac Catalyst' -workspace . | xcpretty
+    xcodebuild test -scheme MetalPetal -destination 'platform=macOS,variant=Mac Catalyst' -workspace . | xcpretty
+else
+    xcodebuild build -scheme MetalPetal -destination 'platform=macOS,variant=Mac Catalyst' -workspace .
+    xcodebuild test -scheme MetalPetal -destination 'platform=macOS,variant=Mac Catalyst' -workspace .
+fi
