8be7b9489953d8e75794b5637bdcbf6075bce871
YuAo <me@imyuao.com>
2020-06-29 13:11:05 +0800

    improve default mask for hexagonal bokeh blur filter

--- Frameworks/MetalPetal/Filters/MTIHexagonalBokehBlurFilter.m
+++ Frameworks/MetalPetal/Filters/MTIHexagonalBokehBlurFilter.m
@@ -58,8 +58,7 @@
     
     MTIMask *mask = self.inputMask;
     if (!mask) {
-        MTIImage *maskImage = [[MTIImage alloc] initWithColor:MTIColorMake(1, 1, 1, 1) sRGB:NO size:CGSizeMake(1, 1)];
-        mask = [[MTIMask alloc] initWithContent:maskImage component:MTIColorComponentRed mode:MTIMaskModeNormal];
+        mask = [[MTIMask alloc] initWithContent:MTIImage.whiteImage component:MTIColorComponentRed mode:MTIMaskModeNormal];
     }
     
     MTIVector * deltas[3];
