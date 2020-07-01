78b50079f59101fac9298ec4e538278609410a14
YuAo <me@imyuao.com>
2020-07-01 21:07:36 +0800

    Support mipmap and cube textures by fallback to MTKTextureLoader

--- Frameworks/MetalPetal/MTITextureLoader.h
+++ Frameworks/MetalPetal/MTITextureLoader.h
@@ -12,7 +12,7 @@ NS_ASSUME_NONNULL_BEGIN
 /// Abstract interface for texture loader.
 @protocol MTITextureLoader <NSObject>
 
-+ (instancetype)newTextureLoaderWithDevice:(id <MTLDevice>)device;
++ (instancetype)newTextureLoaderWithDevice:(id <MTLDevice>)device NS_SWIFT_NAME(makeTextureLoader(device:));
 
 - (nullable id <MTLTexture>)newTextureWithCGImage:(nonnull CGImageRef)cgImage
                                           options:(nullable NSDictionary <MTKTextureLoaderOption, id> *)options
@@ -38,7 +38,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
-/// The default texture loader. A `MTIDefaultTextureLoader` object uses a `MTKTextureLoader` internally to load textures. When an image cannot be loaded with `MTKTextureLoader`, `MTIDefaultTextureLoader` draws the image to a 32bits/pixel BGRA `CVPixelBuffer` and creates a texture from that pixel bufer. Texture loader options `MTKTextureLoaderOptionAllocateMipmaps`, `MTKTextureLoaderOptionGenerateMipmaps` and `MTKTextureLoaderOptionCubeLayout` are not supported.
+/// The default texture loader. A `MTIDefaultTextureLoader` object uses a `MTKTextureLoader` internally to load textures. When an image cannot be loaded with `MTKTextureLoader`, `MTIDefaultTextureLoader` draws the image to a 32bits/pixel BGRA `CVPixelBuffer` and creates a texture from that pixel bufer.
 __attribute__((objc_subclassing_restricted))
 @interface MTIDefaultTextureLoader : NSObject <MTITextureLoader>
 
