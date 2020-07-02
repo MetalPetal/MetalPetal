c9b694eadf517e6c91daceaa26d0a5e3fb109092
YuAo <me@imyuao.com>
2020-07-02 23:46:19 +0800

    Add more texture loader tests

--- Tests/MetalPetalTestHelpers/ImageGenerator.swift
+++ Tests/MetalPetalTestHelpers/ImageGenerator.swift
@@ -14,18 +14,16 @@ import UIKit
 
 extension CGColor {
     static var mti_white: CGColor {
-        #if canImport(UIKit)
-        return UIColor.white.cgColor
-        #else
-        return .white
-        #endif
+        var components: [CGFloat] = [1, 1, 1, 1]
+        return CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: &components)!
     }
     static var mti_black: CGColor {
-        #if canImport(UIKit)
-        return UIColor.black.cgColor
-        #else
-        return .black
-        #endif
+        var components: [CGFloat] = [0, 0, 0, 1]
+        return CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: &components)!
+    }
+    static var mti_r0g128b255: CGColor {
+        var components: [CGFloat] = [0, 128/255.0, 1, 1]
+        return CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: &components)!
     }
 }
 
@@ -84,13 +82,43 @@ public struct ImageGenerator {
         return image
     }
     
-    public static func makeCheckboardImageWithBigEndian() throws -> CGImage {
+    public static func makeR0G128B255CheckboardImageWithBigEndianAlphaLast() throws -> CGImage {
         guard let context = CGContext(data: nil, width: 2, height: 2, bitsPerComponent: 8, bytesPerRow: 2 * 4, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue) else {
             throw Error.cannotCreateCGContext
         }
         context.setFillColor(CGColor.mti_white)
         context.fill(CGRect(x: 0, y: 0, width: 2, height: 2))
-        context.setFillColor(CGColor.mti_black)
+        context.setFillColor(CGColor.mti_r0g128b255)
+        context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        context.fill(CGRect(x: 1, y: 1, width: 1, height: 1))
+        guard let image = context.makeImage() else {
+            throw Error.cannotCreateImageFromContext
+        }
+        return image
+    }
+    
+    public static func makeR0G128B255CheckboardImageWithBigEndianAlphaFirst() throws -> CGImage {
+        guard let context = CGContext(data: nil, width: 2, height: 2, bitsPerComponent: 8, bytesPerRow: 2 * 4, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue) else {
+            throw Error.cannotCreateCGContext
+        }
+        context.setFillColor(CGColor.mti_white)
+        context.fill(CGRect(x: 0, y: 0, width: 2, height: 2))
+        context.setFillColor(CGColor.mti_r0g128b255)
+        context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
+        context.fill(CGRect(x: 1, y: 1, width: 1, height: 1))
+        guard let image = context.makeImage() else {
+            throw Error.cannotCreateImageFromContext
+        }
+        return image
+    }
+    
+    public static func makeR0G128B255CheckboardImageWithDefaultEndianAlphaFirst() throws -> CGImage {
+        guard let context = CGContext(data: nil, width: 2, height: 2, bitsPerComponent: 8, bytesPerRow: 2 * 4, space: CGColorSpaceCreateDeviceRGB(), bitmapInfo: CGImageAlphaInfo.premultipliedFirst.rawValue) else {
+            throw Error.cannotCreateCGContext
+        }
+        context.setFillColor(CGColor.mti_white)
+        context.fill(CGRect(x: 0, y: 0, width: 2, height: 2))
+        context.setFillColor(CGColor.mti_r0g128b255)
         context.fill(CGRect(x: 0, y: 0, width: 1, height: 1))
         context.fill(CGRect(x: 1, y: 1, width: 1, height: 1))
         guard let image = context.makeImage() else {
