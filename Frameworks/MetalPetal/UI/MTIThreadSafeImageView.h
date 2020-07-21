c45a10fbd559819556743d9d7288cca4dfbe95da
Yu Ao <YuAo@users.noreply.github.com>
2020-07-21 20:53:23 +0800

    Fix typo

--- Frameworks/MetalPetal/UI/MTIThreadSafeImageView.h
+++ Frameworks/MetalPetal/UI/MTIThreadSafeImageView.h
@@ -34,7 +34,7 @@ __attribute__((objc_subclassing_restricted))
 
 @property (atomic) MTLClearColor clearColor;
 
-/// This property aliases the olorspace property of the view's CAMetalLayer
+/// This property aliases the colorspace property of the view's CAMetalLayer
 @property (atomic) CGColorSpaceRef colorspace;
 
 @property (atomic) MTIDrawableRenderingResizingMode resizingMode;
