2b105145c1d277cf5e5bd9ff9c0e36eafaa38425
YuAo <me@imyuao.com>
2021-02-02 16:20:39 +0800

    enable YCbCr texture support on Apple Silicon

--- Frameworks/MetalPetal/MTIPixelFormat.m
+++ Frameworks/MetalPetal/MTIPixelFormat.m
@@ -17,11 +17,19 @@ MTLPixelFormat const MTIPixelFormatYCBCR10_420_2P = 505;
 MTLPixelFormat const MTIPixelFormatYCBCR10_420_2P_sRGB = 525;
 
 BOOL MTIDeviceSupportsYCBCRPixelFormat(id<MTLDevice> device) {
-    #if TARGET_OS_TV
-    return [device supportsFeatureSet:MTLFeatureSet_tvOS_GPUFamily2_v1];
-    #elif TARGET_OS_IPHONE && !TARGET_OS_SIMULATOR && !TARGET_OS_MACCATALYST
-    return [device supportsFeatureSet:MTLFeatureSet_iOS_GPUFamily3_v1];
-    #else
-    return NO;
-    #endif
+    if (@available(iOS 13.0, tvOS 13.0, macCatalyst 14.0, macOS 11.0, *)) {
+        if ([device supportsFamily:MTLGPUFamilyApple3]) {
+            return YES;
+        } else {
+            return NO;
+        }
+    } else {
+        #if TARGET_OS_TV
+        return [device supportsFeatureSet:MTLFeatureSet_tvOS_GPUFamily2_v1];
+        #elif TARGET_OS_IPHONE && !TARGET_OS_SIMULATOR && !TARGET_OS_MACCATALYST
+        return [device supportsFeatureSet:MTLFeatureSet_iOS_GPUFamily3_v1];
+        #else
+        return NO;
+        #endif
+    }
 }
