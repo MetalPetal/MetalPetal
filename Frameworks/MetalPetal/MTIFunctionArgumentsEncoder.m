1ec996629c03134802301847ff95856ba834c29f
YuAo <me@imyuao.com>
2021-02-08 15:10:42 +0800

    function arguments encoder improvements, add tests

--- Frameworks/MetalPetal/MTIFunctionArgumentsEncoder.m
+++ Frameworks/MetalPetal/MTIFunctionArgumentsEncoder.m
@@ -118,66 +118,123 @@ __attribute__((objc_subclassing_restricted))
             continue;
         }
         id value = parameters[argument.name];
-        if (value) {
-            if ([value isKindOfClass:[NSValue class]]) {
-                NSValue *nsValue = (NSValue *)value;
-                NSUInteger size;
-                NSGetSizeAndAlignment(nsValue.objCType, &size, NULL);
-                void *valuePtr = malloc(size);
-                [nsValue getValue:valuePtr];
-                @MTI_DEFER {
-                    free(valuePtr);
-                };
-                if (argument.bufferDataSize != size) {
+        if (!value) {
+            continue;
+        }
+        if ([value isKindOfClass:[NSNumber class]]) {
+            NSNumber *number = value;
+            switch (argument.bufferDataType) {
+                case MTLDataTypeBool: {
+                    bool b = (bool)number.boolValue;
+                    NSAssert(sizeof(b) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &b, sizeof(b), argument.index);
+                } break;
+                case MTLDataTypeInt: {
+                    int i = number.intValue;
+                    NSAssert(sizeof(i) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &i, sizeof(i), argument.index);
+                } break;
+                case MTLDataTypeUInt: {
+                    unsigned int i = number.unsignedIntValue;
+                    NSAssert(sizeof(i) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &i, sizeof(i), argument.index);
+                } break;
+                case MTLDataTypeChar: {
+                    char c = number.charValue;
+                    NSAssert(sizeof(c) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &c, sizeof(c), argument.index);
+                } break;
+                case MTLDataTypeUChar: {
+                    unsigned char c = number.unsignedCharValue;
+                    NSAssert(sizeof(c) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &c, sizeof(c), argument.index);
+                } break;
+                case MTLDataTypeShort: {
+                    short s = number.shortValue;
+                    NSAssert(sizeof(s) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &s, sizeof(s), argument.index);
+                } break;
+                case MTLDataTypeUShort: {
+                    unsigned short s = number.unsignedShortValue;
+                    NSAssert(sizeof(s) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &s, sizeof(s), argument.index);
+                } break;
+                case MTLDataTypeFloat: {
+                    float f = number.floatValue;
+                    NSAssert(sizeof(f) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &f, sizeof(f), argument.index);
+                } break;
+                case MTLDataTypeHalf: {
+                    float f = number.floatValue;
+                    __fp16 h = f;
+                    NSAssert(sizeof(h) == argument.bufferDataSize, @"");
+                    MTIArgumentsEncoderEncodeBytes(functionType, encoder, &h, sizeof(h), argument.index);
+                } break;
+                default: {
                     if (inOutError != nil) {
-                        *inOutError = MTIErrorCreate(MTIErrorDataBufferSizeMismatch, (@{@"Argument": argument, @"Value": value}));
+                        *inOutError = MTIErrorCreate(MTIErrorParameterDataTypeNotSupported, (@{@"Argument": argument, @"Value": value}));
+                    }
+                    return NO;
+                } break;
+            }
+        } else if ([value isKindOfClass:[NSValue class]]) {
+            NSValue *nsValue = (NSValue *)value;
+            NSUInteger size;
+            NSGetSizeAndAlignment(nsValue.objCType, &size, NULL);
+            void *valuePtr = malloc(size);
+            [nsValue getValue:valuePtr];
+            @MTI_DEFER {
+                free(valuePtr);
+            };
+            if (argument.bufferDataSize != size) {
+                if (inOutError != nil) {
+                    *inOutError = MTIErrorCreate(MTIErrorDataBufferSizeMismatch, (@{@"Argument": argument, @"Value": value}));
+                }
+                return NO;
+            }
+            MTIArgumentsEncoderEncodeBytes(functionType, encoder, valuePtr, size, argument.index);
+        } else if ([value isKindOfClass:[NSData class]]) {
+            NSData *data = (NSData *)value;
+            MTIArgumentsEncoderEncodeBytes(functionType, encoder, data.bytes, data.length, argument.index);
+        } else if ([value isKindOfClass:[MTIVector class]]) {
+            MTIVector *vector = (MTIVector *)value;
+            MTIArgumentsEncoderEncodeBytes(functionType, encoder, vector.bytes, vector.byteLength, argument.index);
+        } else if ([value isKindOfClass:[MTIDataBuffer class]]) {
+            MTIDataBuffer *dataBuffer = (MTIDataBuffer *)value;
+            id<MTLBuffer> buffer = [dataBuffer bufferForDevice:encoder.device];
+            MTIArgumentsEncoderEncodeBuffer(functionType, encoder, buffer, argument.index);
+        } else {
+            static Class<MTIFunctionArgumentEncoding> SIMDValueEncoder;
+            static dispatch_once_t onceToken;
+            dispatch_once(&onceToken, ^{
+                Class encoder = NSClassFromString(@"MTISIMDArgumentEncoder");
+                if ([encoder conformsToProtocol:@protocol(MTIFunctionArgumentEncoding)]) {
+                    SIMDValueEncoder = encoder;
+                }
+            });
+            if (SIMDValueEncoder) {
+                MTIFunctionArgumentEncodingProxyImplementation *proxy = [[MTIFunctionArgumentEncodingProxyImplementation alloc] initWithEncoder:encoder functionType:functionType argument:argument];
+                NSError *encoderError;
+                [SIMDValueEncoder encodeValue:value argument:argument proxy:proxy error:&encoderError];
+                NSError *error = encoderError ?: proxy.error;
+                if (error) {
+                    if (inOutError != nil) {
+                        *inOutError = error;
                     }
                     return NO;
                 }
-                MTIArgumentsEncoderEncodeBytes(functionType, encoder, valuePtr, size, argument.index);
-            } else if ([value isKindOfClass:[NSData class]]) {
-                NSData *data = (NSData *)value;
-                MTIArgumentsEncoderEncodeBytes(functionType, encoder, data.bytes, data.length, argument.index);
-            } else if ([value isKindOfClass:[MTIVector class]]) {
-                MTIVector *vector = (MTIVector *)value;
-                MTIArgumentsEncoderEncodeBytes(functionType, encoder, vector.bytes, vector.byteLength, argument.index);
-            } else if ([value isKindOfClass:[MTIDataBuffer class]]) {
-                MTIDataBuffer *dataBuffer = (MTIDataBuffer *)value;
-                id<MTLBuffer> buffer = [dataBuffer bufferForDevice:encoder.device];
-                MTIArgumentsEncoderEncodeBuffer(functionType, encoder, buffer, argument.index);
-            } else {
-                static Class<MTIFunctionArgumentEncoding> SIMDValueEncoder;
-                static dispatch_once_t onceToken;
-                dispatch_once(&onceToken, ^{
-                    Class encoder = NSClassFromString(@"MTISIMDArgumentEncoder");
-                    if ([encoder conformsToProtocol:@protocol(MTIFunctionArgumentEncoding)]) {
-                        SIMDValueEncoder = encoder;
-                    }
-                });
-                if (SIMDValueEncoder) {
-                    MTIFunctionArgumentEncodingProxyImplementation *proxy = [[MTIFunctionArgumentEncodingProxyImplementation alloc] initWithEncoder:encoder functionType:functionType argument:argument];
-                    NSError *encoderError;
-                    [SIMDValueEncoder encodeValue:value argument:argument proxy:proxy error:&encoderError];
-                    NSError *error = encoderError ?: proxy.error;
-                    if (error) {
-                        if (inOutError != nil) {
-                            *inOutError = error;
-                        }
-                        return NO;
-                    }
-                    if (!proxy.used) {
-                        [proxy invalidate];
-                        if (inOutError != nil) {
-                            *inOutError = MTIErrorCreate(MTIErrorParameterDataTypeNotSupported, (@{@"Argument": argument, @"Value": value}));
-                        }
-                        return NO;
-                    }
-                } else {
+                if (!proxy.used) {
+                    [proxy invalidate];
                     if (inOutError != nil) {
                         *inOutError = MTIErrorCreate(MTIErrorParameterDataTypeNotSupported, (@{@"Argument": argument, @"Value": value}));
                     }
                     return NO;
                 }
+            } else {
+                if (inOutError != nil) {
+                    *inOutError = MTIErrorCreate(MTIErrorParameterDataTypeNotSupported, (@{@"Argument": argument, @"Value": value}));
+                }
+                return NO;
             }
         }
     }
