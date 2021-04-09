7e825fc7a17e7653b2226dd39940093b95502f48
YuAo <me@imyuao.com>
2021-04-09 15:24:44 +0800

    terminate example app after last window closed

--- MetalPetalExamples/Shared/MetalPetalExamplesApp.swift
+++ MetalPetalExamples/Shared/MetalPetalExamplesApp.swift
@@ -9,6 +9,17 @@ import SwiftUI
 
 @main
 struct MetalPetalExamplesApp: App {
+    
+    #if os(macOS)
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    class AppDelegate: NSObject, NSApplicationDelegate {
+        func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+            true
+        }
+    }
+    #endif
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
